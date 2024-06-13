01c3b9045c3bcad8a403abe92818af34883156ca
Alex-717 <liangyuhao717@163.com>
2024-06-13 14:31:54 +0800

    打包

--- build/rollup.build.config.js
+++ build/rollup.build.config.js
@@ -3,24 +3,57 @@ import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
-// import { babel } from '@rollup/plugin-babel'
+import { babel } from '@rollup/plugin-babel'
 import { srcRoot, projectRoot } from '../utils/path.js'
-import path from 'path'
+import path, { resolve } from 'path'
 
-const resolve = path.resolve
+const formatMap = {
+  esm: 'esm',
+  es: 'esm',
+  cjs: 'common',
+  umd: 'umd'
+}
+const input = resolve(srcRoot, './index.js')
 
-export default defineConfig({
-  input: resolve(srcRoot, './index.js'),
-  output: {
-    format: 'iife',
-    name: 'scriptFailedReloadLib',
-    // file: resolve(projectRoot, './dist/bundle.js')
-    file: resolve(projectRoot, './test/public/js/bundle.js')
-  },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    // babel()
-  ]
-})
+const buildConfig = function (options) {
+  const { browser = true, minify = false, ...config } = options
+  return [{
+    input,
+    output: {
+      format: config.format,
+      name: config.name,
+      file: resolve(projectRoot, `./dist/bundle.${formatMap[config.format]}.js`)
+    },
+    plugins: [
+      nodeResolve({ browser }),
+      commonjs(),
+
+      ...config.plugins
+    ]
+  }]
+}
+
+export default [
+  ...buildConfig({
+    format: 'esm',
+    plugins: [
+
+    ]
+  })
+]
+
+// export default defineConfig({
+//   input: resolve(srcRoot, './index.js'),
+//   output: {
+//     format: 'iife',
+//     name: 'scriptFailedReloadLib',
+//     // file: resolve(projectRoot, './dist/bundle.js')
+//     file: resolve(projectRoot, './test/public/js/bundle.js')
+//   },
+//   plugins: [
+//     nodeResolve(),
+//     commonjs(),
+//     // babel()
+//   ]
+// })
 
